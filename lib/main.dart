f5e1a03457b8799c4ad41f4a5aaf669f2b90fefa
Jithu Kailas <jithukailas1998@gmail.com>
2020-10-19 09:16:50 +0530

    hello

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hello/app.dart';
+import 'package:hello/observer.dart';
 
 void main() {
+  Bloc.observer = BeerBlocObserver();
   runApp(MyApp());
 }
 
